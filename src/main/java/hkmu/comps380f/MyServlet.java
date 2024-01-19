fef95e0cc662e6154fe8e9812de78408effdd7b0
cskeith <cslklee@gmail.com>
2024-01-19 14:22:59 +0800

    Lab 2 Task 1 completed

---
+++ src/main/java/hkmu/comps380f/MyServlet.java
@@ -0,0 +1,26 @@
+package hkmu.comps380f;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().println("Hello, World!");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started.");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has stopped.");
+    }
+}
