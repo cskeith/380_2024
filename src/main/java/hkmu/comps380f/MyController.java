ebf527db362ee6da6baa8ef543354d7daaa432cb
cskeith <cslklee@gmail.com>
2024-03-01 12:06:57 +0800

    Lab 8 Task 3 completed

--- src/main/java/hkmu/comps380f/MyController.java
+++ src/main/java/hkmu/comps380f/MyController.java
@@ -1,8 +1,14 @@
 package hkmu.comps380f;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Controller
 public class MyController {
@@ -20,5 +26,31 @@ public class MyController {
     @PostMapping("/checkboxes")
     public String multiValueResult() {
         return "MultiValueResult";
+    }
+
+    @GetMapping("/do/*")
+    public String recordSessionActivity(HttpServletRequest request, HttpSession session) {
+        if (session.getAttribute("activity") == null)
+            session.setAttribute("activity", new CopyOnWriteArrayList<PageVisit>());
+
+        @SuppressWarnings("unchecked")
+        CopyOnWriteArrayList<PageVisit> visits
+                = (CopyOnWriteArrayList<PageVisit>) session.getAttribute("activity");
+        if (!visits.isEmpty()) {
+            PageVisit last = visits.get(visits.size() - 1);
+            last.setLeftTimestamp(System.currentTimeMillis());
+        }
+
+        PageVisit now = new PageVisit();
+        now.setEnteredTimestamp(System.currentTimeMillis());
+        if (request.getQueryString() == null) now.setRequest(request.getRequestURL().toString());
+        else now.setRequest(request.getRequestURL() + "?" + request.getQueryString());
+        try {
+            now.setIpAddress(InetAddress.getByName(request.getRemoteAddr()));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        visits.add(now);
+        return "viewSessionActivity";
     }
 }
