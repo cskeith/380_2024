101b6d4051c2c401eee44a1b8a8ba4737c4b39b9
cskeith <cslklee@gmail.com>
2024-03-06 13:29:10 +0800

    Lab 9 created project

---
+++ src/main/java/hkmu/comps380f/Lab09Application.java
@@ -0,0 +1,13 @@
+package hkmu.comps380f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab09Application.class, args);
+    }
+
+}
