542f28467339b2ba94d11ea01bf4632bd217d501
cskeith <cslklee@gmail.com>
2024-03-13 11:19:43 +0800

    Spring Boot project

---
+++ src/main/java/hkmu/comps380f/HelloSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package hkmu.comps380f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringHibernateApplication.class, args);
+    }
+
+}
