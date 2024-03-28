a09e126f6c2977ac07b6ee845cea4ecd1de8f989
cskeith <cslklee@gmail.com>
2024-03-28 17:59:00 +0800

    Lab 12 Task 1 completed

--- src/main/java/hkmu/comps380f/controller/UserManagementController.java
+++ src/main/java/hkmu/comps380f/controller/UserManagementController.java
@@ -2,12 +2,13 @@ package hkmu.comps380f.controller;
 
 import hkmu.comps380f.dao.UserManagementService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
@@ -25,8 +26,15 @@ public class UserManagementController {
     }
 
     public static class Form {
+        @NotEmpty(message="Please enter your user name.")
         private String username;
+
+        @NotEmpty(message="Please enter your password.")
+        @Size(min=6, max=15, message="Your password length must be between {min} and {max}.")
         private String password;
+        private String confirm_password;
+
+        @NotEmpty(message="Please select at least one role.")
         private String[] roles;
 
         // getters and setters for all properties
@@ -46,6 +54,14 @@ public class UserManagementController {
             this.password = password;
         }
 
+        public String getConfirm_password() {
+            return confirm_password;
+        }
+
+        public void setConfirm_password(String confirm_password) {
+            this.confirm_password = confirm_password;
+        }
+
         public String[] getRoles() {
             return roles;
         }
@@ -61,7 +77,11 @@ public class UserManagementController {
     }
 
     @PostMapping("/create")
-    public String create(Form form) throws IOException {
+    public String create(@ModelAttribute("ticketUser") @Valid Form form, BindingResult result)
+            throws IOException {
+        if (result.hasErrors()) {
+            return "addUser";
+        }
         umService.createTicketUser(form.getUsername(),
                 form.getPassword(), form.getRoles());
         return "redirect:/user/list";
--- src/main/webapp/WEB-INF/jsp/addUser.jsp
+++ src/main/webapp/WEB-INF/jsp/addUser.jsp
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head><title>Customer Support</title></head>
+<head>
+    <title>Customer Support</title>
+    <style>
+        .error {
+            color: red;
+            font-weight: bold;
+            display: block;
+        }
+    </style>
+</head>
 <body>
 <c:url var="logoutUrl" value="/logout"/>
 <form action="${logoutUrl}" method="post">
@@ -11,10 +20,16 @@
 <h2>Create a User</h2>
 <form:form method="POST" modelAttribute="ticketUser">
     <form:label path="username">Username</form:label><br/>
+    <form:errors path="username" cssClass="error" />
     <form:input type="text" path="username"/><br/><br/>
     <form:label path="password">Password</form:label><br/>
+    <form:errors path="password" cssClass="error" />
     <form:input type="text" path="password"/><br/><br/>
+    <form:label path="confirm_password">Confirm Password</form:label><br/>
+    <form:errors path="confirm_password" cssClass="error" />
+    <form:input type="text" path="confirm_password" /><br/><br/>
     <form:label path="roles">Roles</form:label><br/>
+    <form:errors path="roles" cssClass="error" />
     <form:checkbox path="roles" value="ROLE_USER"/>ROLE_USER
     <form:checkbox path="roles" value="ROLE_ADMIN"/>ROLE_ADMIN
     <br/><br/>
